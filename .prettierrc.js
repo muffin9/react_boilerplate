f90607f4ba57b3dbac2ba0b8cf6263849b10fec5
Muffin <jinlog9@gmail.com>
2022-11-17 22:18:59 +0900

    🛠 Setting 관련 전체 수정.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   printWidth: 80, // 코드 한줄이 maximum 80칸
-  tabWidth: 2, // 들여쓰기 너비는 2칸
+  tabWidth: 4, // 들여쓰기 너비는 2칸
   useTabs: false, // 탭의 사용을 금하고 스페이스바 사용으로 대체하게 formatting
   semi: true, // 코드 마지막에 세미콜론
   singleQuote: true, // 문자열은 따옴표로 formatting
